4e8f64fb57d0066a6e18fe37c08eb75f22e38422
Anderson Santos <andersonacpemp@gmail.com>
2025-04-02 13:53:04 -0300

    Add coment in models/cliente

--- database/models/cliente.py
+++ database/models/cliente.py
@@ -2,7 +2,7 @@ from peewee import Model, CharField, DateTimeField
 from database.database import db
 from datetime import datetime
 
-
+#Modelo dos clientes
 class Cliente(Model):
     nome = CharField()
     email = CharField()
